87b7e6d4a7dd0ea353168af5f1e00c73c8eaca8d
VinayChavan2006 <154125564+VinayChavan2006@users.noreply.github.com>
2025-04-18 23:12:02 +0530

    Update processor.v

--- processor.v
+++ processor.v
@@ -26,11 +26,12 @@ module processor (
 );
 
     // Internal signals
-    wire [31:0] pc, next_pc, instr, reg_read1, reg_read2, imm_ext, alu_src_muxout, alu_out, mem_read_data, write_data;
+    wire [31:0] pc, next_pc,float_reg_read1, float_reg_read2, instr, reg_read1, reg_read2, imm_ext, alu_src_muxout, alu_out, mem_read_data, write_data;
     wire [31:0] jump_addr, branch_addr, pc_plus_4;
     wire [4:0] write_reg;
     wire [5:0] alu_ctl;
-    wire RegDst, AluSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, Jump, Jr, Jal, Zero, InvZero;
+    wire [5:0] flu_ctl;
+    wire RegDst, AluSrc, FloatOp,ALUorFLU, MemtoReg, RegWrite,FloatRegWrite, MemRead, MemWrite, Branch, Jump, Jr, Jal, Zero, InvZero;
 
     // Program Counter
     PC pc_inst (
@@ -49,18 +50,25 @@ module processor (
     // Control Unit
     ControlUnit control_inst (
         .opcode(instr[31:26]),
+        .funct(instr[5:0]),
         .RegDst(RegDst),
         .AluSrc(AluSrc),
         .MemtoReg(MemtoReg),
-        .RegWrite(RegWrite),
+        .IntRegWrite(IntRegWrite),
+        .FloatRegWrite(FloatRegWrite),
         .MemRead(MemRead),
         .MemWrite(MemWrite),
         .Branch(Branch),
         .Jump(Jump),
         .Jr(Jr),
-        .Jal(Jal)
+        .Jal(Jal),
+        .FloatOp(FloatOp),
+        .ALUorFLU(ALUorFLU)
     );
-
+    // Register File Selection
+    assign reg_read1 = FloatOp ? float_reg_read1 : reg_read1;
+    assign reg_read2 = FloatOp ? float_reg_read2 : reg_read2;
+    
     // Register File
     mux2_1 #(.W(5)) reg_dst_mux (
         .A(instr[20:16]), // rt
@@ -78,6 +86,18 @@ module processor (
         .reg_write(RegWrite | Jal),
         .read_data1(reg_read1),
         .read_data2(reg_read2)
+    );
+    
+    // Floating-Point Register File
+    float_register_file float_reg_file_inst (
+        .clk(clk),
+        .read_reg1(instr[25:21]),
+        .read_reg2(instr[20:16]),
+        .write_reg(write_reg),
+        .write_data(write_data),
+        .reg_write(FloatRegWrite),
+        .read_data1(float_reg_read1),
+        .read_data2(float_reg_read2)
     );
 
     // Sign Extend
@@ -104,6 +124,7 @@ module processor (
 
     // ALU
     ALU alu_inst (
+        .opcode(instr[31:26]),
         .A(reg_read1),
         .B(alu_src_muxout),
         .AluCtl(alu_ctl),
@@ -111,7 +132,21 @@ module processor (
         .AluOut(alu_out),
         .Zero(Zero)
     );
-
+    // FLU
+    FLU flu_inst (
+        .A(reg_read1),
+        .B(reg_read2),
+        .flu_ctl(flu_ctl),
+        .flu_out(flu_out)
+    );
+    
+    // ALU/FLU Mux
+    mux2_1 #(.W(32)) alu_flu_mux (
+        .A(alu_out),
+        .B(flu_out),
+        .Sel(ALUorFLU),
+        .mux_out(alu_flu_out)
+    );
     // Data Memory
     data_memory data_mem_inst (
         .addr(alu_out),
@@ -130,8 +165,8 @@ module processor (
     );
 
     // PC Update Logic
-    assign pc_plus_4 = pc + 4;
-    assign branch_addr = pc_plus_4 + (imm_ext << 2);
+    assign pc_plus_4 = pc + 1;
+    assign branch_addr = pc_plus_4 + (imm_ext);
     assign jump_addr = {6'b000000,instr[25:0]};
 //    assign jump_addr = {pc[31:28], instr[25:0], 2'b00};
 
@@ -171,66 +206,49 @@ module instruction_memory (
 );
     reg [31:0] instr_mem [0:1023];
     initial begin
-        // Sample program:
-        // addi $s0, $zero, 5    : 100001 00000 00001 0000000000000101
-        // add  $s1, $s0, $s0    : 000000 00001 00001 00010 00000 000001
-        // beq  $s0, $s1, +4     : 101101 00001 00010 0000000000000001
-        // j    0x4              : 010001 00000000000000000000000100
+//        addi $t0, $zero, 0
+//        addi $t1, $zero, 6
+//        addi $t2, $zero, 1
+//        bgte $t2, $t0, 18
+//        add $t3, $t1, $t2
+//        lw $t4, 0($t3)
+//        addi $t5, $t2, -1
+//        blt $t5, $zero, 6
+//        add $t6, $t1, $t5
+//        lw $t7, 0($t6)
+//        bleq $t7, $t4, 3
+//        sw $t7, 1($t6)
+//        addi $t5, $t5, -1
+//        j 7
+//        add $t8, $t1, $t5
+//        sw $t4, 1($t8)
+//        addi $t2, $t2, 1
+//        j 3
+        
+        instr_mem[0]  = 32'b10000100000010100000000000000000; 
+        instr_mem[1]  = 32'b10000100000010010000000000000110; 
+        instr_mem[2]  = 32'b10000100000010110000000000000001; 
+        instr_mem[3]  = 32'b10110001011010010000000000010010; 
+        instr_mem[4]  = 32'b00000001010010110110000000000001; 
+        instr_mem[5]  = 32'b10011001100011010000000000000000; 
+        instr_mem[6]  = 32'b10000101011011101111111111111111; 
+        instr_mem[7]  = 32'b10110101110000000000000000000110; 
+        instr_mem[8]  = 32'b00000001010011100111100000000001; 
+        instr_mem[9]  = 32'b10011001111100000000000000000000; 
+        instr_mem[10] = 32'b10111010000011010000000000000011; 
+        instr_mem[11] = 32'b10011101111100000000000000000001; 
+        instr_mem[12] = 32'b10000101110011101111111111111111; 
+        instr_mem[13] = 32'b01000100000000000000000000000111; 
+        instr_mem[14] = 32'b00000001010011101000100000000001; 
+        instr_mem[15] = 32'b10011110001011010000000000000001; 
+        instr_mem[16] = 32'b10000101011010110000000000000001; 
+        instr_mem[17] = 32'b01000100000000000000000000000011; 
+        instr_mem[18] = 32'b00000000000000000000000000000000;
 
-        //         # main:
-        // addi $s0, $zero, 0x1000  # 10000100000000010000010000000000
-        // addi $s1, $zero, 6       # 10000100000000100000000000000110
-        // addi $s2, $zero, 1       # 10000100000000110000000000000001
-        // # outer_loop:
-        // beq $s2, $zero, 12       # 10110100011000000000000000001100
-        // addi $s2, $zero, 0       # 10000100000000110000000000000000
-        // addi $s3, $zero, 0       # 10000100000001000000000000000000
-        // sub $s4, $s1, $s2        # 00000000010000110010100000000011
-        // # inner_loop:
-        // bgt $s3, $s4, -8         # 10111100100001011111111111111000
-        // sll $t0, $s3, 2          # 10011000100010010000000000000010
-        // add $t1, $s0, $t0        # 00000000001010010101000000000001
-        // lw $t2, 0($t1)           # 10101001010010110000000000000000
-        // lw $t3, 4($t1)           # 10101001010011000000000000000100
-        // bgt $t2, $t3, 2          # 10111101011011000000000000000010
-        // j -6                     # 01000111111111111111111111111010
-        // # swap:
-        // sw $t3, 0($t1)           # 10101101010011000000000000000000
-        // sw $t2, 4($t1)           # 10101101010010110000000000000100
-        // addi $s2, $zero, 1       # 10000100000000110000000000000001
-        // # next:
-        // addi $s3, $s3, 1         # 10000100100001000000000000000001
-        // j -10                    # 01000111111111111111111111110110
-        // # done:
-        // j 0                      # 01000100000000000000000000000000
-        instr_mem[0]  = 32'b10000100000000010001000000000000; // addi $s0, $zero, 0x1000
-        instr_mem[1]  = 32'b10000100000000100000000000000110; // addi $s1, $zero, 6
-        instr_mem[2]  = 32'b10000100000000110000000000000001; // addi $s2, $zero, 1
-        instr_mem[3]  = 32'b11000000011000100000000000001100; // bge $s2, $s1, 12
-        instr_mem[4]  = 32'b10011001010000110000000000000010; // sll $t1, $s2, 2
-        instr_mem[5]  = 32'b00000000001010100101000000000001; // add $t1, $s0, $t1
-        instr_mem[6]  = 32'b10101001010010010000000000000000; // lw $t0, 0($t1)
-        instr_mem[7]  = 32'b10000100011001001111111111111111; // addi $s3, $s2, -1
-        instr_mem[8]  = 32'b11000100100000000000000000001001; // blt $s3, $zero, 9
-        instr_mem[9]  = 32'b10011000000001000101000010000000; // sll $t1, $s3, 2
-        instr_mem[10] = 32'b00000000001010100101000000000001; // add $t1, $s0, $t1
-        instr_mem[11] = 32'b10101001010010110000000000000000; // lw $t2, 0($t1)
-        instr_mem[12] = 32'b10111101011010010000000000000010; // bgt $t2, $t0, 2
-        instr_mem[13] = 32'b01000100000000000000000000100100; // j 0x44
-        instr_mem[14] = 32'b10101101010010110000000000000100; // sw $t2, 4($t1)
-        instr_mem[15] = 32'b10000100100001001111111111111111; // addi $s3, $s3, -1
-        instr_mem[16] = 32'b01000100000000000000000000100000; // j 0x20
-        instr_mem[17] = 32'b10011000000001000101000010000000; // sll $t1, $s3, 2
-        instr_mem[18] = 32'b00000000001010100101000000000001; // add $t1, $s0, $t1
-        instr_mem[19] = 32'b10000101010010100000000000000100; // addi $t1, $t1, 4
-        instr_mem[20] = 32'b10101101010010010000000000000000; // sw $t0, 0($t1)
-        instr_mem[21] = 32'b10000100011000110000000000000001; // addi $s2, $s2, 1
-        instr_mem[22] = 32'b01000100000000000000000000001100; // j 0x0C
-        instr_mem[23] = 32'b01000100000000000000000000101100; // j 0x5C
 
     end
     always @(addr) begin
-        instr = instr_mem[addr >> 2];
+        instr = instr_mem[addr];
     end
 endmodule
 
@@ -271,20 +289,20 @@ module data_memory (
     input mem_write, mem_read,
     output reg [31:0] read_data
 );
-    reg [31:0] data_mem [0:2047];
-    initial begin
-        data_mem[32'h1000 >> 2] = 32'd6;
-        data_mem[32'h1004 >> 2] = 32'd3;
-        data_mem[32'h1008 >> 2] = 32'd8;
-        data_mem[32'h100C >> 2] = 32'd1;
-        data_mem[32'h1010 >> 2] = 32'd9;
-        data_mem[32'h1014 >> 2] = 32'd2;
-    end
+    reg [31:0] data_mem [0:1023];
+//    initial begin
+//        data_mem[32'h0] = 32'd6;
+//        data_mem[32'h1] = 32'd3;
+//        data_mem[32'h2] = 32'd8;
+//        data_mem[32'h3] = 32'd1;
+//        data_mem[32'h4] = 32'd9;
+//        data_mem[32'h5] = 32'd2;
+//    end
     always @(*) begin
         if (mem_write)
-            data_mem[addr >> 2] = write_data;
+            data_mem[addr] = write_data;
         if (mem_read)
-            read_data = data_mem[addr >> 2];
+            read_data = data_mem[addr];
     end
 endmodule
 
@@ -292,19 +310,21 @@ endmodule
 module ALU (
     input [31:0] A, B,
     input [5:0] AluCtl,
+    input [5:0] opcode,
     input InvZero,
+    output reg [31:0] hi,lo,
     output reg [31:0] AluOut,
     output Zero
 );
-    reg [31:0] hi, lo;
+//    reg [31:0] hi, lo;
     reg [63:0] temp;
     always @(*) begin
-        hi = hi; lo = lo; // Retain values unless updated
+        hi = 32'h0; lo = 32'h0; // Retain values unless updated
         case (AluCtl)
             6'd1:  AluOut = A + B;                    // add, addu, addi, addiu
             6'd2:  AluOut = A - B;                    // sub, subu
-            6'd3:  begin temp = A * B; hi = temp[63:32]; lo = temp[31:0]; AluOut = lo; end // mul
-            6'd4:  begin temp = A * B; hi = hi + temp[63:32]; lo = lo + temp[31:0]; AluOut = lo; end // madd, maddu
+            6'd3:  begin temp = $signed(A) * $signed(B); hi = temp[63:32]; lo = temp[31:0]; AluOut = lo; end // mul
+            6'd4:  begin temp = $signed(A) * $signed(B); hi = hi + temp[63:32]; lo = lo + temp[31:0]; AluOut = lo; end // madd, maddu
             6'd5:  AluOut = A << B[4:0];              // sll
             6'd6:  AluOut = A >> B[4:0];              // srl
             6'd7:  AluOut = $signed(A) >>> B[4:0];    // sra
@@ -313,21 +333,23 @@ module ALU (
             6'd10: AluOut = A & B;                    // and, andi
             6'd11: AluOut = A ^ B;                    // xor, xori
             6'd12: AluOut = ~A;                       // not
-            6'd13: AluOut = (A < B) ? 32'd1 : 32'd0;  // slt, slti
-            6'd14: AluOut = A;                        // lui (pass A, but shifted in data path)
+            6'd13: AluOut = (opcode == 6'b101111 || opcode == 6'b110000) ? 
+                            ($unsigned(A) < $unsigned(B) ? 32'd1 : 32'd0) : 
+                            ($signed(A) < $signed(B) ? 32'd1 : 32'd0); // slt, slti, bgt, blt, bleu, bgtu
+            6'd14: AluOut = A << 16;                        // lui (pass A, but shifted in data path)
             6'd15: AluOut = (A == B) ? 32'd1 : 32'd0; // seq
-            
+            6'd16: AluOut = $signed(A) > $signed(B) ? 32'd1 : 32'd0; //sgt
             
             default: AluOut = 32'd0;
         endcase
     end
-    assign Zero = InvZero ? (AluOut != 0) : (AluOut == 0);
+    assign Zero = InvZero == 1'b1 ? (AluOut != 0) : (AluOut == 0);
 endmodule
 
 // ALU Control
 module ALUControl (
     input [5:0] opcode, funct,
-    output reg [5:0] alu_ctl,
+    output reg [5:0] alu_ctl,flu_ctl,
     output reg inv_zero
 );
     always @(*) begin
@@ -346,6 +368,11 @@ module ALUControl (
                 6'd10: alu_ctl = 6'd12; // not
                 6'd11: alu_ctl = 6'd11; // xor
                 6'd12: alu_ctl = 6'd13; // slt
+                6'd13: alu_ctl = 6'd5; // sll
+                6'd14: alu_ctl = 6'd6; // srl
+                6'd15: alu_ctl = 6'd8; // sla
+                6'd16: alu_ctl = 6'd7; // sra
+                
                 default: alu_ctl = 6'd0;
             endcase
         end
@@ -356,30 +383,26 @@ module ALUControl (
                 5'd3:  alu_ctl = 6'd10; // andi
                 5'd4:  alu_ctl = 6'd9;  // ori
                 5'd5:  alu_ctl = 6'd11; // xori
-                5'd6:  alu_ctl = 6'd5;  // sll
-                5'd7:  alu_ctl = 6'd6;  // srl
-                5'd8:  alu_ctl = 6'd8;  // sla
-                5'd9:  alu_ctl = 6'd7;  // sra
-                5'd10: alu_ctl = 6'd1;  // lw
-                5'd11: alu_ctl = 6'd1;  // sw
-                5'd12: alu_ctl = 6'd14; // lui
-                5'd13: begin alu_ctl = 6'd2; inv_zero = 0; end // beq
-                5'd14: begin alu_ctl = 6'd2; inv_zero = 1; end // bne
-                5'd15: begin alu_ctl = 6'd13; inv_zero = 1; end // bgt
-                5'd16: begin alu_ctl = 6'd13; inv_zero = 0; end // bgte
-                5'd17: begin alu_ctl = 6'd13; inv_zero = 0; end // blt
-                5'd18: begin alu_ctl = 6'd13; inv_zero = 1; end // bleq
-                5'd19: begin alu_ctl = 6'd13; inv_zero = 1; end // bleu
-                5'd20: begin alu_ctl = 6'd13; inv_zero = 1; end // bgtu
-                5'd21: alu_ctl = 6'd13; // slti
-                5'd22: alu_ctl = 6'd15; // seq
+                5'd6: alu_ctl = 6'd1;  // lw
+                5'd7: alu_ctl = 6'd1;  // sw
+                5'd8: alu_ctl = 6'd14; // lui
+                5'd9: begin alu_ctl = 6'd2; inv_zero = 0; end // beq
+                5'd10: begin alu_ctl = 6'd2; inv_zero = 1; end // bne
+                5'd11: begin alu_ctl = 6'd16; inv_zero = 1; end // bgt
+                5'd12: begin alu_ctl = 6'd13; inv_zero = 0; end // bgte
+                5'd13: begin alu_ctl = 6'd13; inv_zero = 0; end // blt
+                5'd14: begin alu_ctl = 6'd13; inv_zero = 1; end // bleq
+                5'd15: begin alu_ctl = 6'd13; inv_zero = 1; end // bleu
+                5'd16: begin alu_ctl = 6'd13; inv_zero = 1; end // bgtu
+                5'd17: alu_ctl = 6'd13; // slti
+                5'd18: alu_ctl = 6'd15; // seq
 
                 default: alu_ctl = 6'd0;
             endcase
         end
         else if (opcode[5:4] == 2'b00) begin
             case(opcode[3:0])
-                4'b0001: alu_ctl = 6'd1; // mfc1
+                4'b0001: flu_ctl = 6'd2; // sub.s
                 
             endcase
         end
@@ -391,88 +414,67 @@ endmodule
 // Control Unit
 module ControlUnit (
     input [5:0] opcode,
-    output reg RegDst, AluSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, Jump, Jr, Jal
+    input [5:0] funct,
+    output reg RegDst, AluSrc, MemtoReg, IntRegWrite, FloatRegWrite, MemRead, MemWrite, Branch, Jump, Jr, Jal, FloatOp, ALUorFLU
 );
     always @(*) begin
-        RegDst = 0; AluSrc = 0; MemtoReg = 0; RegWrite = 0;
+        RegDst = 0; AluSrc = 0; MemtoReg = 0; IntRegWrite = 0; FloatRegWrite = 0;
         MemRead = 0; MemWrite = 0; Branch = 0; Jump = 0; Jr = 0; Jal = 0;
+        FloatOp = 0; ALUorFLU = 0;
         casez (opcode)
-            6'b000000: begin // R-type
+            6'b000000: begin // R-type (integer)
                 RegDst = 1;
-                RegWrite = 1;
+                IntRegWrite = 1;
+                ALUorFLU = 0;
+            end
+            6'b000010: begin // fsub
+                RegDst = 1;
+                FloatRegWrite = 1;
+                FloatOp = 1;
+                ALUorFLU = 1;
             end
             6'b100001: begin // addi
                 AluSrc = 1;
-                RegWrite = 1;
+                IntRegWrite = 1;
+                ALUorFLU = 0;
             end
-            6'b100010: begin // addiu
-                AluSrc = 1;
-                RegWrite = 1;
-            end
-            6'b100011: begin // andi
-                AluSrc = 1;
-                RegWrite = 1;
-            end
-            6'b100100: begin // ori
-                AluSrc = 1;
-                RegWrite = 1;
-            end
-            6'b100101: begin // xori
-                AluSrc = 1;
-                RegWrite = 1;
-            end
-            6'b100110: begin // sll
-                AluSrc = 1;
-                RegWrite = 1;
-            end
-            6'b100111: begin // srl
-                AluSrc = 1;
-                RegWrite = 1;
-            end
-            6'b101000: begin // sla
-                AluSrc = 1;
-                RegWrite = 1;
-            end
-            6'b101001: begin // sra
-                AluSrc = 1;
-                RegWrite = 1;
-            end
-            6'b101010: begin // lw
+            6'b100110: begin // lw
                 AluSrc = 1;
                 MemtoReg = 1;
-                RegWrite = 1;
+                IntRegWrite = 1;
                 MemRead = 1;
+                ALUorFLU = 0;
             end
-            6'b101011: begin // sw
+            6'b100111: begin // sw
                 AluSrc = 1;
                 MemWrite = 1;
+                ALUorFLU = 0;
             end
-            6'b101100: begin // lui
-                AluSrc = 1;
-                RegWrite = 1;
+            6'b101100: begin // bgte
+                Branch = 1;
+                ALUorFLU = 0;
             end
-            6'b101101: Branch = 1; // beq
-            6'b101110: Branch = 1; // bne
-            6'b101111: Branch = 1; // bgt
-            6'b110000: Branch = 1; // bgte
-            6'b110001: Branch = 1; // blt
-            6'b110010: Branch = 1; // bleq
-            6'b110011: Branch = 1; // bleu
-            6'b110100: Branch = 1; // bgtu
-            6'b110101: begin // slti
-                AluSrc = 1;
-                RegWrite = 1;
+            6'b101101: begin // blt
+                Branch = 1;
+                ALUorFLU = 0;
             end
-            6'b110110: begin // seq
-                AluSrc = 1;
-                RegWrite = 1;
+            6'b101110: begin // bleq
+                Branch = 1;
+                ALUorFLU = 0;
             end
-            6'b010001: Jump = 1; // j
-            6'b010010: Jr = 1;   // jr
-            6'b010011: Jal = 1;  // jal
+            6'b010001: begin // j
+                Jump = 1;
+                ALUorFLU = 0;
+            end
+            6'b010011: begin // jal
+                Jal = 1;
+                IntRegWrite = 1;
+                ALUorFLU = 0;
+            end
             default: begin
-                RegDst = 0; AluSrc = 0; MemtoReg = 0; RegWrite = 0;
+                RegDst = 0; AluSrc = 0; MemtoReg = 0; IntRegWrite = 0; FloatRegWrite = 0;
                 MemRead = 0; MemWrite = 0; Branch = 0; Jump = 0; Jr = 0; Jal = 0;
+                FloatOp = 0; ALUorFLU = 0;
             end
         endcase
     end
